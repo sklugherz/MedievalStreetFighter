5ecb7de0b256fa742c018550215761e983ce2457
sklugherz <sklugherz@gmail.com>
2024-06-16 19:26:10 -0600

    fkit

---
+++ characters.py
@@ -0,0 +1,16 @@
+characters = [
+    {
+        "name": "Elven Warrior",
+        "animationSteps": [10, 8, 1, 7, 7, 3, 7],
+        "size": 162, #pixels
+        "scale": 4,
+        "offset": [72,56]
+    },
+    {
+        "name": "Dark Wizard",
+        "animationSteps": [8, 8, 1, 8, 8, 3, 7],
+        "size": 250,
+        "scale": 3,
+        "offset": [112, 1-7]
+    }
+]
