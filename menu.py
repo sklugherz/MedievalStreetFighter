587d747fe39ec5b9a3bda57d4912f7925c97b2c4
sklugherz <sklugherz@gmail.com>
2024-06-20 11:58:52 -0600

    menu loop

--- menu.py
+++ menu.py
@@ -4,6 +4,7 @@ import sys
 from game import Game
 from fighter import Fighter
 from button import Button
+from constants import MENU_ORANGE, MENU_WHITE, WHITE
 
 mixer.init()
 pygame.init()
@@ -64,12 +65,36 @@ def main_menu():
     while True:
         draw_bg(menu_bg)
         mouse_pos = pygame.mouse.get_pos()
-        PLAY_BUTTON = Button(play_rect, pos=(640, 250), 
-                            text_input="PLAY", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
-        OPTIONS_BUTTON = Button(options_rect, pos=(640, 400), 
-                            text_input="OPTIONS", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
-        QUIT_BUTTON = Button(quit_rect, pos=(640, 550), 
-                            text_input="QUIT", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
 
+        MENU_TEXT = get_font(100).render("MAIN MENU", True, MENU_ORANGE)
+        MENU_RECT = MENU_TEXT.get_rect(center=(SCREEN_WIDTH / 2, 100))
+
+        PLAY_BUTTON = Button(play_rect, pos=(SCREEN_WIDTH / 2, 250), 
+                            text_input="PLAY", font=get_font(75), base_color=MENU_WHITE, hovering_color=WHITE)
+        OPTIONS_BUTTON = Button(options_rect, pos=(SCREEN_WIDTH / 2, 400), 
+                            text_input="OPTIONS", font=get_font(75), base_color=MENU_WHITE, hovering_color=WHITE)
+        QUIT_BUTTON = Button(quit_rect, pos=(SCREEN_WIDTH / 2, 550), 
+                            text_input="QUIT", font=get_font(75), base_color=MENU_WHITE, hovering_color=WHITE)
+
+        screen.blit(MENU_TEXT, MENU_RECT)
+
+        for button in [PLAY_BUTTON, OPTIONS_BUTTON, QUIT_BUTTON]:
+            button.changeColor(mouse_pos)
+            button.update(screen)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if PLAY_BUTTON.checkForInput(mouse_pos):
+                    play()
+                if OPTIONS_BUTTON.checkForInput(mouse_pos):
+                    options()
+                if QUIT_BUTTON.checkForInput(mouse_pos):
+                    pygame.quit()
+                    sys.exit()
+
+        pygame.display.update()
 
 main_menu()
