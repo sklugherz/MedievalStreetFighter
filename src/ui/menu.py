695f74447f2a2ed31cec8d74872d2c6008c61ccc
sklugherz <sklugherz@gmail.com>
2025-03-11 14:39:10 -0700

    impoort update

--- src/ui/menu.py
+++ src/ui/menu.py
@@ -1,6 +1,6 @@
 import pygame
 import sys
-from ui.button import Button
+from button import Button
 from constants.colors import MENU_ORANGE, MENU_WHITE, WHITE
 from core.helper import draw_bg, get_font
 from constants.event import Event
